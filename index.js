a54653b1ef807e40d898101bc4657a0ee775ae06
AntiSociai <regan.nalder@gmail.com>
2017-04-25 18:09:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(window).load(function(){
+  $('.effect').each(function(){
+    $(this).addClass('active');
+  })
+  $('.fade').addClass('active');
+})
+
+$(document).ready(function() {  
+  $('.cta').click(function(){
+    $('html, body').animate({
+      scrollTop: $("#anchor").offset().top
+    }, 1000);
+  });
+});
